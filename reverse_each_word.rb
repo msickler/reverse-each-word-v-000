b4cb403672a6e095aae3474bab5d8d842d420320
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-28 23:01:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence)
-  array = sentence.split(" ").each do |word| word.reverse!
+  array = sentence.split(" ").collect do |word| word.reverse!
       end
       array.join(" ")
-       array
         end
